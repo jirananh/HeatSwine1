f46813a4d6cb8008b07df2858b2b176aaae8435d
jiranan <jirananh@betagro.com>
2024-04-29 16:01:13 +0700

    finish 29-4-23

---
+++ android/app/src/main/kotlin/com/ji/formanimal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ji.formanimal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utility/app_controller.dart
@@ -0,0 +1,6 @@
+import 'package:formanimal/models/swine_code_model.dart';
+import 'package:get/get.dart';
+
+class AppController extends GetxController {
+  RxList swineCodeModels = <SwineCodeModel>[].obs;
+}
